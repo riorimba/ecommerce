a340a8fe04186a09a7215a057138b37ffe17711c
riorimba <monhero78@gmail.co>
2024-12-11 14:23:58 +0700

    User Auth API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::get('me', [UserController::class, 'me'])->middleware('auth:sanctum');
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
