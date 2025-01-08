18c94e10d6043a4748101b8ca1c69670a941fc60
riorimba <monhero78@gmail.co>
2025-01-08 13:56:23 +0700

    fix: notification order status

--- app/Notifications/OrderStatusChangedNotification.php
+++ app/Notifications/OrderStatusChangedNotification.php
@@ -11,14 +11,14 @@ class OrderStatusChangedNotification extends Notification
 {
     use Queueable;
 
-    protected $id;
+    protected $order_id;
     protected $status;
     /**
      * Create a new notification instance.
      */
-    public function __construct($id, $status)
+    public function __construct($order_id, $status)
     {
-        $this->id = $id;
+        $this->order_id = $order_id;
         $this->status = $status;
     }
 
@@ -35,8 +35,8 @@ class OrderStatusChangedNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            'message' => 'Order ID: ' . $this->id . ' status has been changed to ' . $this->status,
-            'url' => url('/orders/' . $this->id),
+            'message' => 'Order ID: ' . $this->order_id . ' status has been changed to ' . $this->status,
+            'url' => url('/orders/' . $this->order_id),
         ];
     }
 }
